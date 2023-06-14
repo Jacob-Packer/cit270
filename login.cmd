e53c30aefe4735adacdb696864aa21f6d495d421
Jacob-Packer <pac20007@byui.edu>
2023-06-14 13:26:49 -0600

    hi

--- login.cmd
+++ login.cmd
@@ -2,6 +2,6 @@
 echo "Logging in"
 
 @REM curl -v -d "@login.json" POST -H "Content-Type:application/json" https://dev.stedi.me/login
-curl -v -d "@login.json" POST -H "Content-Type:application/json" http://localhost:3000/login
+curl -v -d "@login.json" POST -H "Content-Type:application/json" https://jacobpacker.cit270.com/
 
 @REM curl -v https://dev.stedi.me/validate/6af0c312-148e-4ea0-b15b-fbe0f87d45aa
